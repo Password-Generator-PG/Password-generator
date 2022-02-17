0a5a7c209e940b1c1e2497082e74675ad6623059
K+ <www.kplus69@gmail.com>
2022-02-17 15:18:38 +0100

    sw2

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,6 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new workbox.strategies.NetworkFirst()
-);
-import {registerRoute} from 'workbox-routing';
-import {StaleWhileRevalidate} from 'workbox-strategies';
-
-registerRoute(
-  ({request}) => request.destination === 'script' ||
-                 request.destination === 'style',
-  new StaleWhileRevalidate()
-);
-import {CacheableResponsePlugin} from 'workbox-cacheable-response';
-import {CacheFirst} from 'workbox-strategies';
-import {ExpirationPlugin} from 'workbox-expiration';
-import {registerRoute} from 'workbox-routing';
-
-registerRoute(
-  ({request}) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  }),
 );

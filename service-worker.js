765775f8caf1fa7cfa0ab2b35859e46fb58ee446
K+ <www.kplus69@gmail.com>
2022-02-17 15:10:41 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -4,3 +4,31 @@ workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new workbox.strategies.NetworkFirst()
 );
+import {registerRoute} from 'workbox-routing';
+import {StaleWhileRevalidate} from 'workbox-strategies';
+
+registerRoute(
+  ({request}) => request.destination === 'script' ||
+                 request.destination === 'style',
+  new StaleWhileRevalidate()
+);
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {CacheFirst} from 'workbox-strategies';
+import {ExpirationPlugin} from 'workbox-expiration';
+import {registerRoute} from 'workbox-routing';
+
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);

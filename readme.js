59b43bc903bc4de56c260021b47ad290ce40d807
K+ <www.kplus69@gmail.com>
2022-03-24 20:06:18 +0100

    st 1

--- readme.js
+++ readme.js
@@ -1,9 +1,4 @@
 //warn
 console.warn("%cIf you find any bugs or errors please report them on GitHub.", "color: #32e56f");
 //Version
-console.log("%cPassword Generator (PG)  v. 1.1.9", "color: #32e56f");
-//features
-console.groupCollapsed("%cNew features:", "color: #32e56f");
-console.log("%cNew features are on GitHub from now.", "color: #05ab6d");
-console.log("%chttps://github.com/K-plus69/Password-generator", "color: #05ab6d");
-console.groupEnd("%cNew features:", "color: #32e56f");
+console.log("%cPassword Generator (PG)  v. 1.2.0", "color: #32e56f");

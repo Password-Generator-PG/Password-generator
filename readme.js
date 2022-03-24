cf3b05aa7069f8f37dac0b26f42c704a1e613497
K+ <www.kplus69@gmail.com>
2022-03-24 21:06:20 +0100

    v1.2.1

--- readme.js
+++ readme.js
@@ -1,4 +1,4 @@
 //warn
 console.warn("%cIf you find any bugs or errors please report them on GitHub.", "color: #32e56f");
 //Version
-console.log("%cPassword Generator (PG)  v. 1.2.0", "color: #32e56f");
+console.log("%cPassword Generator (PG)  v. 1.2.1", "color: #32e56f");

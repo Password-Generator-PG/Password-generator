cedcb72085a44973a7758691c1a4bb5bf062562c
K+ <www.kplus69@gmail.com>
2022-03-28 20:42:42 +0200

    e

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-import { GenerateSW } from 'workbox-webpack-plugins';
 
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',

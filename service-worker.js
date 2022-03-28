1f25c558e04b21ad882bf13076b29e61a6f8e403
K+ <www.kplus69@gmail.com>
2022-03-28 21:09:44 +0200

    s

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox
 
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
-  new workbox.strategies.NetworkFirst({
+  new workbox.strategies.CacheFirst({
     cacheName: 'google-fonts'
   })
 );
 
 workbox.routing.registerRoute(
     new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
+    workbox.strategies.CacheFirst({
       cacheName: 'google-fonts',
     }),
   );

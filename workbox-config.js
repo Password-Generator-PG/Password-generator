69eb176e689d8f8bc5924e64fb92cf4145739905
K+ <www.kplus69@gmail.com>
2022-03-28 19:45:33 +0200

    s

--- workbox-config.js
+++ workbox-config.js
@@ -3,10 +3,10 @@ module.exports = {
 	globPatterns: [
 		'**/*.{css,html,svg,jpg,png,json,js,ttf,md,jpeg,webp}'
 	],
-	swDest: 'sw.js',
+	swDest: 'service-worker.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
-	"swSrc": 'service-worker.js'
+	swSrc: 'service-worker.js'
 };

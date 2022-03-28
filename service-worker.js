082b8c71015bd84bfc34776253d4df57b1f43bf8
K+ <www.kplus69@gmail.com>
2022-03-28 21:08:01 +0200

    s

--- service-worker.js
+++ service-worker.js
@@ -13,14 +13,6 @@ workbox.routing.registerRoute(
     new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
     workbox.strategies.cacheFirst({
       cacheName: 'google-fonts',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30,
-        }),
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200]
-        }),
-      ],
     }),
   );
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);

2a64f65854f73db10f9099549888e6974440fd07
K+ <www.kplus69@gmail.com>
2022-03-29 15:11:52 +0200

    s

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-
+import { registerRoute, Route } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'))
   new workbox.strategies.CacheFirst({
     cacheName: 'google-fonts'
   });
+  const imageRoute = new Route(({ request, sameOrigin }) => {
+  return sameOrigin && request.destination === 'image'
+}, new CacheFirst());
+
+// Register the new route
+registerRoute(imageRoute);
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);

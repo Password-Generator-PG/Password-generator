a959f1ed818c30de4b2faee7c92b952839623799
K+ <www.kplus69@gmail.com>
2022-04-10 13:21:18 +0200

    s

--- script2.js
+++ script2.js
@@ -394,30 +394,17 @@ window.location.reload();
 }
 }
 //notifications
+if ('Notification' in window && navigator.serviceWorker) {
+  // Display the UI to let the user toggle notifications
+}
 Notification.requestPermission(function(status) {
-	alert3.style.visibility = "visible";
-	document.getElementById("alert2").innerHTML = "Notifications: " , status;
+	console.log(status);
 });
 
 function displayNotification() {
   if (Notification.permission == 'granted') {
     navigator.serviceWorker.getRegistration().then(function(reg) {
-      var options = {
-        body: 'Here is a notification body!',
-        icon: 'images/example.png',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: 1
-        },
-        actions: [
-          {action: 'explore', title: 'Explore this new world',
-            icon: 'images/checkmark.png'},
-          {action: 'close', title: 'Close notification',
-            icon: 'images/xmark.png'},
-        ]
-      };
-      reg.showNotification('Hello world!', options);
+      reg.showNotification('Hello world!');
     });
   }
 };
